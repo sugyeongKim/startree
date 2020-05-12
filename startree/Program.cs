92bc53a81035f9f4fda759bdb99a0f3da017ef6b
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-12 13:36:53 +0900

    6번 별찍기

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,16 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            int j, n = 8;
+            for (int i = 0; i < n; i++) 
+            {
+                for (j = n - 1; j > i; j--) 
+                    Console.Write(" ");
+                for (j = 0; j < i * 2 + 1; j++) 
+                    Console.Write("*");
+
+                Console.WriteLine("");
+            }
         }
     }
 }
